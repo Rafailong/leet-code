9d0e33d2cd7da3f5ad881889fc85b04d9d611d88
rafael.avila@ally.com <rafael.avila@mastercard.com>
2021-11-04 11:25:46 -0600

    https://leetcode.com/problems/palindrome-number/ - without using strings

--- src/main/scala/PalindromeNumber.scala
+++ src/main/scala/PalindromeNumber.scala
@@ -4,11 +4,35 @@ object PalindromeNumber {
     s1 == s1.reverse
   }
 
+  def isPalindrome(x: Int): Boolean = {
+
+    if (x < 0) false
+    else {
+      var arr = Array.empty[Int]
+      var arr1 = Array.empty[Int]
+      var n = x
+
+      while (n > 0) {
+        val a = n % 10
+        arr = arr.appended(a)
+        arr1 = arr1.prepended(a)
+
+        n = n / 10
+      }
+
+      arr.sameElements(arr1)
+    }
+  }
+
   def main(args: Array[String]): Unit = {
     assert(isPalindromeBruteForce(121))
-
     assert(!isPalindromeBruteForce(-121))
     assert(!isPalindromeBruteForce(10))
     assert(!isPalindromeBruteForce(-101))
+
+    assert(isPalindrome(121))
+    assert(!isPalindrome(-121))
+    assert(!isPalindrome(10))
+    assert(!isPalindrome(-101))
   }
 }
