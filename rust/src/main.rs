e65cd68bc1ca5b25d095381f5760a0ed93d406e7
rafael.avila@ally.com <rafael.avila@mastercard.com>
2021-11-11 16:02:08 -0600

    https://leetcode.com/problems/remove-element/

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -2,10 +2,31 @@ fn main() {
     println!("Hello, world!");
 
     // let mut nums = [1, 1, 2].to_vec();
-    let mut nums = [0, 0, 1, 1, 1, 2, 2, 3, 3, 4].to_vec();
-    let k = remove_duplicates(&mut nums);
+    // let mut nums = [0, 0, 1, 1, 1, 2, 2, 3, 3, 4].to_vec();
+    // let k = remove_duplicates(&mut nums);
+
+    // https://leetcode.com/problems/remove-element/
+    let mut nums = [3, 2, 2, 3].to_vec();
+    let val = 3;
+    let k = remove_elements(&mut nums, val);
     println!("k = {}", k);
     println!("nums after removal: {:?}", nums);
+}
+
+// https://leetcode.com/problems/remove-element/
+pub fn remove_element(nums: &mut Vec<i32>, val: i32) -> i32 {
+    let mut nums_size = nums.len();
+    let mut i = 0;
+    while i < nums_size {
+        if nums[i] == val {
+            nums.remove(i);
+            nums_size -= 1;
+        } else {
+            i += 1;
+        }
+    }
+
+    i as i32
 }
 
 pub fn remove_duplicates(nums: &mut Vec<i32>) -> i32 {
